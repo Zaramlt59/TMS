22bd8048a828ea9913be93a2c4c366096fe0ce04
Zaramlt59 <71581171+Zaramlt59@users.noreply.github.com>
2025-09-17 11:45:05 +0530

    Delete src directory

--- src/controllers/index.ts
+++
@@ -1,7 +0,0 @@
-export { districtController } from './districtController.js';
-export { rdBlockController } from './rdBlockController.js';
-export { villageController } from './villageController.js';
-export { schoolController } from './schoolController.js';
-export { teacherController } from './teacherController.js';
-export { subjectController } from './subjectController.js';
-export { masterDataController } from './masterDataController.js';
